cc03273c460a27c51b39283b7d788eb309e0b178
Shu Gao <sgao7@lsu.edu>
2020-12-07 16:09:45 -0600

    Update Target_Recognition_v1.2.py

--- Code/Target_Recognition_v1.2.py
+++ Code/Target_Recognition_v1.2.py
@@ -1,5 +1,5 @@
 __author__ = 'Shu Gao'
-# Target Recongnation Loop Method
+# Target Recognition Loop Method
 #########################import modeules##########################
 import arcpy,os, fnmatch
 from arcpy import env
